24bfa6b231bb817a4335755297d80eced63fbe27
Raul Glogovetan <raul@glogovetan.com>
2024-02-14 00:19:03 +0200

    Refactor: SEGMENT project handler to be more resilient

--- src/routes/segment/projects.ts
+++ src/routes/segment/projects.ts
@@ -1,14 +1,14 @@
 import dotenv from 'dotenv';
 import { Options } from 'http-proxy-middleware';
 import { Response, Request } from 'express-serve-static-core';
-import { IncomingMessage } from 'http';
+import { IncomingMessage, ClientRequest } from 'http';
 
 // Needs to be ignored since it doesn't have types and TS goe crazy
 //@ts-ignore
 import modifyResponse from 'node-http-proxy-json';
 
-import { logger, getStaticOrigin } from '~helpers';
-import { ContentTypes, Headers,} from '~types';
+import { logger, getStaticOrigin, sendResponse } from '~helpers';
+import { ContentTypes, Headers, HttpStatuses, ResponseTypes } from '~types';
 import { ExternalUrls } from '~constants';
 
 dotenv.config();
@@ -22,20 +22,39 @@ export const segmentProjectsProxyRouteHandler: Options = {
   // path includes the api key, which is the index used to access
   // the project settings
   pathRewrite: () => `/v1/projects/${process.env.SEGMENT_WRITE_KEY}/settings`,
+  onProxyReq: (proxyRequest: ClientRequest) => {
+    if (!process.env.SEGMENT_WRITE_KEY) {
+      proxyRequest.abort();
+    }
+  },
   onProxyRes: (proxyResponse: IncomingMessage, request: Request, response: Response) => {
     proxyResponse.headers[Headers.AllowOrigin] = getStaticOrigin(
       request.headers.origin,
     );
     proxyResponse.headers[Headers.PoweredBy] = 'Colony';
+
+    if (proxyResponse.statusCode !== HttpStatuses.OK) {
+      logger(`Could not proxy SEGMENT settings for project "${process.env.SEGMENT_WRITE_KEY}". The service will not function until this can be retrieved`);
+      return sendResponse(response, request, {
+        message: 'Cannot fetch project',
+        type: ResponseTypes.Error,
+        data: '',
+      }, HttpStatuses.BAD_REQUEST);
+    }
+
     modifyResponse(response, proxyResponse, (body: Record<string, any>) => {
-      if (body) {
-        // Don't return the API key to the client
-        // so that it doesn't get exposed
-        delete body.integrations['Segment.io'].apiKey;
+      try {
+        if (body) {
+          // Don't return the API key to the client
+          // so that it doesn't get exposed
+          delete body.integrations['Segment.io'].apiKey;
+        }
+      } catch (error) {
+        proxyResponse.destroy();
       }
       return body;
     });
-    logger(`Proxied SEGMENT settings for project ${process.env.SEGMENT_WRITE_KEY}`);
+    logger(`Proxied SEGMENT settings for project "${process.env.SEGMENT_WRITE_KEY}"`);
   },
   logProvider: () => ({
     log: logger,
